3cdcbc99144fb507a7580c8d7356c1bae23367e8
Pavan khalyan <pavankhalyan2004@gmail.com>
2024-03-08 22:40:24 +0530

    initial commit

---
+++ src/assets/lib/data.tsx
@@ -0,0 +1,536 @@
+import canvaicon from "../../assets/icons/canvaicon.svg";
+import figmaicon from "../../assets/icons/figmaicon.svg";
+import htmlicon from "../../assets/icons/htmlicon.svg";
+import cssicon from "../../assets/icons/cssicon.svg";
+import javascripticon from "../../assets/icons/javascripticon.svg";
+import typescripticon from "../../assets/icons/typescripticon.svg";
+import reacticon from "../../assets/icons/reacticon.svg";
+import nextjsicon from "../../assets/icons/nextjsicon.svg";
+import nodejsicon from "../../assets/icons/nodejsicon.svg";
+import tailwindcssicon from "../../assets/icons/tailwindicon.svg";
+import mongodbicon from "../../assets/icons/mongoicon.svg";
+import vuejsicon from "../../assets/icons/vueicon.svg";
+import expressicon from "../../assets/icons/expressicon.svg";
+import sassscssicon from "../../assets/icons/sassicon.svg";
+import trelloicon from "../../assets/icons/trelloicon.svg";
+import apiicon from "../../assets/icons/apiicon.svg";
+import axiosicon from "../../assets/icons/axiosicon.svg";
+import spotifyicon from "../../assets/icons/spotifyicon.svg";
+import netlifyicon from "../../assets/icons/netlifyicon.svg";
+import rendericon from "../../assets/icons/rendericon.svg";
+import jwticon from "../../assets/icons/jwticon.svg";
+import wordpressicon from "../../assets/icons/wordpressicon.svg";
+import shopifyicon from "../../assets/icons/shopifyicon.svg";
+import webflowicon from "../../assets/icons/webflowicon.svg";
+import tastyMockup from "../../assets/img/tasty_mockup.webp";
+import instorMockup from "../../assets/img/instor_mockup.webp";
+import silentmoonMockup from "../../assets/img/silentmoon_mockup.webp";
+import profilepicture from "../img/me2.jpg";
+import caricon from "../../assets/icons/car-icon.svg";
+import travelicon from "../../assets/icons/travel-icon.svg";
+import hardwareicon from "../../assets/icons/hardware-icon.svg";
+import java from "../../assets/icons/java.svg";
+import c from "../../assets/icons/c.svg";
+import python from "../../assets/icons/python.svg";
+import nextjsiconwhite from "../../assets/icons/nextjsiconwhite.svg";
+import ae from "../../assets/icons/after-effects-1.svg";
+import ppc from "../../assets/icons/premiere-pro-cc.svg";
+import ps from "../../assets/icons/adobe-photoshop-2.svg";
+import expressiconwhite from "../../assets/icons/expressiconwhite.svg";
+import { GoHome, GoPerson, GoMail, GoStack, GoProject } from "react-icons/go";
+import { FiGithub, FiLink, FiLinkedin, FiMail } from "react-icons/fi";
+import Imprint from "../../components/Imprint";
+import Privacy from "../../components/Privacy";
+
+export const headerIntroData = {
+  title: {
+    en: "HI, I'M PAVAN KHALYAN",
+  },
+  subtitle: "FULLSTACK DEVELOPER",
+  description: {
+    en: " I'm Pavan Khalyan S, a fullstack developer with the goal of advancing my career and participating in inspiring projects. Here I present my work and my passion for web development. Let's shape digital solutions together and shape the future!",
+  },
+  buttons: [
+    {
+      name: "Contact",
+      label: {
+        en: "Contact me",
+      },
+      icon: FiMail,
+      color: "main-btn",
+    },
+    {
+      name: "Projects",
+      label: {
+        en: "My Projects",
+      },
+      icon: FiGithub,
+      color: "secondary-btn",
+    },
+  ],
+  profilepicture: profilepicture,
+} as const;
+
+export const projectsData = [
+  {
+    title: "",
+    description:
+      "",
+    description_EN:
+      "",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      ,
+      { name: "React", icon: reacticon },
+      ,
+      ,
+      { name: "MongoDB", icon: mongodbicon },
+      { name: "Express", icon: expressiconwhite },
+      { name: "Node.js", icon: nodejsicon },
+      { name: "Figma", icon: figmaicon },
+      ,
+    ],
+    image: silentmoonMockup,
+    deploymenturl: "",
+    githuburl:
+      "",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#FFD5BD",
+    },
+  },
+  {
+    title: "",
+    description:
+      "",
+    description_EN:
+      "",
+    technologies: [
+      { name: "Html", icon: htmlicon },
+      { name: "CSS", icon: cssicon },
+      { name: "JavaScript", icon: javascripticon },
+      { name: "React", icon: reacticon },
+      { name: "JWT & Bcrypt", icon: jwticon },
+      { name: "Axios", icon: axiosicon },
+      { name: "MongoDB", icon: mongodbicon },
+      { name: "Express", icon: expressiconwhite },
+      { name: "Node.js", icon: nodejsicon },
+      { name: "Figma", icon: figmaicon },
+      { name: "Render", icon: rendericon },
+      { name: "Trello", icon: trelloicon },
+    ],
+    image: instorMockup,
+    deploymenturl: "",
+    githuburl: "",
+    githubicon: FiGithub,
+    deploymenticon: FiLink,
+    colors: {
+      main: "main-btn",
+      second: "secondary-btn",
+      icon: "white",
+      projectcolor: "#E3964A",
+    },
+  },
+] as const;
+
+export const liveTickerData = {
+  content: {
+    en: "More Projects on Github",
+  },
+} as const;
+
+export const skillsDataWeb = [
+  {
+    skillsTitle: "WebDevelopment",
+    skills: [
+      {
+        title: "HTML",
+        hash: "#html",
+        icon: htmlicon,
+        color: "#F1662A",
+      },
+      {
+        title: "CSS",
+        hash: "#CSS",
+        icon: cssicon,
+        color: "#1572B6",
+      },
+      {
+        title: "JavaScript",
+        hash: "#JavaScript",
+        icon: javascripticon,
+        color: "#F7DF1E",
+      },
+      {
+        title: "TypeScript",
+        hash: "#TypeScript",
+        icon: typescripticon,
+        color: "#007ACC",
+      },
+      {
+        title: "React",
+        hash: "#React",
+        icon: reacticon,
+        color: "#61DAFB",
+      },
+      {
+        title: "Node.js",
+        hash: "#Node.js",
+        icon: nodejsicon,
+        color: "#339933",
+      },
+      {
+        title: "Tailwind",
+        hash: "#Tailwind",
+        icon: tailwindcssicon,
+        color: "#38B2AC",
+      },
+      {
+        title: "MongoDB",
+        hash: "#MongoDB",
+        icon: mongodbicon,
+        color: "#449C45",
+      },
+      {
+        title: "Express",
+        hash: "#Express",
+        icon: [expressicon, expressiconwhite],
+        color: ["#000000", "#FFFFFF"],
+      }
+    ],
+  },
+] as const;
+
+export const skillsDataDesign = [
+  {
+    skillsTitle: "Other Languages",
+    skills: [
+      { title: "C", hash: "#C", icon: c, color: "#1572B6" },
+      {
+        title: "Java",
+        hash: "#Java",
+        icon: java,
+        color: "#F1662A",
+      },
+      { title: "Python", hash: "#python", icon: python, color: "#F7DF1E" },
+    ],
+  },
+] as const;
+
+export const skillsDataCMS = [
+  {
+    skillsTitle: "OTHER SKILLS",
+    skills: [
+      {
+        title: "PhotoShop",
+        hash: "#photoshop",
+        icon: ps,
+        color: "#1572B6",
+      },
+      {
+        title: "Premiere Pro",
+        hash: "#premierepro",
+        icon: ppc,
+        color: "#1572B6",
+      },
+      {
+        title: "After Effects",
+        hash: "#aftereffects",
+        icon: ae,
+        color: "#1572B6",
+      },
+    ],
+  },
+] as const;
+
+export const navLinks = [
+  { en: "Home", hash: "#home", icon: GoHome },
+  { en: "Skills", hash: "#skills", icon: GoStack },
+  { en: "Projects", hash: "#projects", icon: GoProject },
+  { en: "About me", hash: "#about-me", icon: GoPerson },
+  { en: "Contact", hash: "#contact", icon: GoMail },
+] as const;
+
+export const FooterLinks = [
+  // {  en: "Imprint", hash: "#imprint", data: <Imprint /> },
+  // {  en: "Privacy", hash: "#privacy", data: <Privacy /> },
+] as const;
+
+export const sideBarRightMail = {
+  link: "mailto:pavankhalyan2004@gmail.com",
+  text: "pavankhalyan2004@gmail.com",
+} as const;
+
+export const sideBarLeftSocials = [
+  {
+    link: "https://linkedin.com/in/pavan-khalyan-478a33259",
+    icon: FiLinkedin,
+    altimgname: "linkedin",
+  },
+  {
+    link: "https://github.com/pavankhalyan",
+    icon: FiGithub,
+    altimgname: "github",
+  },
+  {
+    link: "mailto:myemail@com",
+    icon: FiMail,
+    altimgname: "mail",
+  },
+] as const;
+
+export const quotesData = [
+  {
+    en: `"NOTHING GREAT COME EASY.YOU CAN ACHIEVE ANYTHING WITH OBSESSION AND PERSISTENCE"`,
+  },
+  {
+    en: `"ALL THAT LOVE YOU GAVE ME,THAT'S WHAT MADE ME"`,
+  },
+] as const;
+
+export const aboutMeData = {
+
+  title_EN: "About me",
+  paragraphs_EN: [
+    {
+      title: "ABOUT ME ",
+      description:
+        "I'm Pavan Khalyan from Electronics Communication Engineering,Sri Eshwar College Of Engineering.And I finished my schooling in St.Joseph.Mat.Hr.Sec.School in Hosur ",
+      icon: hardwareicon,
+    },
+    {
+      title: "On the Fast Lane of Life",
+      description:
+        "Besides coding, am really passionate in doing projects and extracurricular activities like video editing,videographer and photographer",
+      icon: caricon,
+    },
+    {
+      title: "The Joy of Discovery as a Life Motto",
+      description:
+        "My journey as a web developer is one part of my life path. I live by the motto that the adventure only begins when you leave the familiar behind. Discovering new places and cultures is my form of creative inspiration.",
+      icon: travelicon,
+    },
+  ],
+};
+
+export const contactData = {
+  title: {
+    en: "Contact",
+  },
+  description: {
+    en: "Write me a message and I will get back to you.",
+  },
+  inputfields: [
+    {
+      name: "name",
+      placeholder: {
+        en: "Your Name",
+      },
+      type: "text",
+      validation: {
+        en: "Please fill in your name",
+      },
+      pattern: "{2}",
+    },
+    {
+      name: "email",
+      placeholder: {
+        en: "Your E-Mail",
+      },
+      type: "email",
+      validation: {
+        en: "Please fill in your email",
+      },
+      pattern: "[@]{4}",
+    },
+    {
+      name: "subject",
+      placeholder: {
+        en: "Your Subject",
+      },
+      type: "text",
+      validation: {
+        en: "Please fill in your subject",
+      },
+      pattern: "{10}",
+    },
+  ],
+  textarea: {
+    placeholder: {
+      en: "Your Message",
+    },
+    name: "message",
+    rows: 10,
+    validation: {
+      en: "Please fill in your message",
+    },
+    pattern: "{10}",
+  },
+  button: {
+    value: {
+      en: "Send",
+    },
+  },
+  icon: FiMail,
+  iconcolor: "#FFFFFF",
+  colors: {
+    main: "main-btn",
+    second: "secondary-btn",
+    icon: "white",
+  },
+  privacyOptIn: {
+    checkbox: {
+      de: "Ich stimme zu, dass Alpay meine personenbezogenen Daten (Name und E-Mail-Adresse) verwenden darf, um mit mir Kontakt aufzunehmen.",
+      en: "",
+    },
+    description: {
+      de: "Durch Übermittlung dieser Anfrage bestätigen Sie, dass Sie die Datenschutzerklärung gelesen haben",
+      en: "",
+    },
+  },
+} as const;
+
+export const toastMessages = {
+  loadingProject: {
+    en: "🦄 The live demo will open shortly. Starting servers...",
+  },
+  successEmailSent: {
+    en: "🦄 Thank you for your email. I will get back to you as soon as possible",
+  },
+  failedEmailSent: {
+    en: "🦄 Unfortunately the sending of your email did not work. Please try again later",
+  },
+  failedValidationName: {
+    en: "Please fill in your name",
+  },
+} as const;
+
+export const buttonLabels = {
+  language: {
+    en: "EN",
+  },
+} as const;
+
+export const directionStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginRight: "4rem",
+  },
+  "outer-left-to-inner-right": {
+    marginLeft: "4rem",
+    transform: "scaleX(-1)",
+  },
+  "inner-right-to-middle": {
+    width: "100%",
+    transform: "scaleY(1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  "inner-left-to-middle": {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  "middle-to-inner-right": {
+    width: "100%",
+    transform: "scale(1,-1)",
+  },
+  "middle-to-inner-left": {
+    width: "100%",
+    transform: "scale(-1,-1)",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  middle: {
+    width: "100%",
+    transform: "scaleX(-1)",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+};
+
+export const heightStyles: Record<string, { heights: [string, string] }> = {
+  small: {
+    heights: ["25rem", "15rem"],
+  },
+  middle: {
+    heights: ["35rem", "25rem"],
+  },
+  large: {
+    heights: ["45rem", "35rem"],
+  },
+  extraLarge: {
+    heights: ["55rem", "45rem"],
+  },
+};
+
+export const spaceStyles: Record<string, React.CSSProperties> = {
+  "outer-right-to-inner-left": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "outer-left-to-inner-right": {
+    marginTop: "-6rem",
+    width: "100%",
+  },
+  "inner-right-to-middle": {
+    marginTop: "-20rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "inner-left-to-middle": {
+    marginTop: "-10rem",
+    width: "50%",
+    zIndex: "-10",
+  },
+  "middle-to-inner-right": {
+    width: "75%",
+  },
+  "middle-to-inner-left": {
+    marginTop: "-2.5rem",
+    width: "50%",
+  },
+  middle: {
+    marginTop: "-2.5rem",
+    width: "0%",
+    display: "none",
+  },
+};
+
+export const widthStyles: Record<string, { widths: [string, string] }> = {
+  "outer-right-to-inner-left": {
+    widths: ["74.45%", "74.45%"],
+  },
+  "outer-left-to-inner-right": {
+    widths: ["75%", "75%"],
+  },
+  "inner-right-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "inner-left-to-middle": {
+    widths: ["50.1%", "49%"],
+  },
+  "middle-to-inner-right": {
+    widths: ["33.4%", "50.03%"],
+  },
+  "middle-to-inner-left": {
+    widths: ["50.1%", "49%"],
+  },
+  middle: {
+    widths: ["0%", "0%"],
+  },
+};
